6c1802e23f872fb99468c4e9de6ec6479a0fc2e0
Wolfgang Resch <wresch@ciq.com>
2025-03-25 15:07:36 -0400

    new example

---
+++ level1/breaking-a-carbon-nanotube/unbreakable-bonds/stagein.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+# adapted from https://github.com/lammpstutorials/lammpstutorials-inputs
+# This job runs the md part of the second lennard jones liquid example
+set -ex
+
+run="${1}"
+mkdir -p "${run}" && cd "${run}" || exit 1
+
+commit="4e249a6f7a0f8f8057bf72c07d39841a297e69a6"
+url="https://raw.githubusercontent.com/lammpstutorials/lammpstutorials-inputs/${commit}"
+curl -s "${url}/level1/breaking-a-carbon-nanotube/unbreakable-bonds/input.lammps" > input.lammps
+curl -s "${url}/level1/breaking-a-carbon-nanotube/unbreakable-bonds/cnt_molecular.data" > cnt_molecular.data
+curl -s "${url}/level1/breaking-a-carbon-nanotube/unbreakable-bonds/parm.lammps" > parm.lammps
