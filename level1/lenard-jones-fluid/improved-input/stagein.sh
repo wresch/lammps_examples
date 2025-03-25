77ed8586414d89aeaff04d25774bb2c4ece81b80
Wolfgang Resch <wresch@ciq.com>
2025-03-25 14:34:41 -0400

    fixed commit hash in download scripts

--- level1/lenard-jones-fluid/improved-input/stagein.sh
+++ level1/lenard-jones-fluid/improved-input/stagein.sh
@@ -6,7 +6,7 @@ set -ex
 run="${1}"
 mkdir -p "${run}" && cd "${run}" || exit 1
 
-commit="e249a6f7a0f8f8057bf72c07d39841a297e69a6"
+commit="4e249a6f7a0f8f8057bf72c07d39841a297e69a6"
 url="https://raw.githubusercontent.com/lammpstutorials/lammpstutorials-inputs/${commit}"
 curl -s "${url}/level1/lennard-jones-fluid/improved-input/input.md.lammps" > input.lammps
 curl -s "${url}/level1/lennard-jones-fluid/improved-input/minimized_coordinate.data" > minimized_coordinate.data
--- level1/lenard-jones-fluid/my-first-input/stagein.sh
+++ level1/lenard-jones-fluid/my-first-input/stagein.sh
@@ -3,8 +3,12 @@
 set -ex
 
 run="${1}"
-mkdir -p "${run}" && cd "${run}" || exit 1
+mkdir -p "${run}" && cd "${run}"
 
-commit="e249a6f7a0f8f8057bf72c07d39841a297e69a6"
+commit="4e249a6f7a0f8f8057bf72c07d39841a297e69a6"
 url="https://raw.githubusercontent.com/lammpstutorials/lammpstutorials-inputs/${commit}"
 curl -s "${url}/level1/lennard-jones-fluid/my-first-input/input.lammps" > input.lammps
+
+pwd
+ls -lh
+cat input.lammps
