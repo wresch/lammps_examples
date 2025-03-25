6dd44255a44f709d0da9d4a26fc2abc98b0f36cd
Wolfgang Resch <wresch@ciq.com>
2025-03-25 09:43:46 -0400

    initial commit

---
+++ level1/lenard-jones-fluid/my-first-input/setup.sh
@@ -0,0 +1,6 @@
+#! /bin/sh
+
+mkdir run
+cd run
+wget "https://raw.githubusercontent.com/lammpstutorials/lammpstutorials-inputs/refs/heads/main/level1/lennard-jones-fluid/my-first-input/input.lammps"
+
