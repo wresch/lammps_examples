52814665f7a367e274093904638990198043d70f
Wolfgang Resch <wresch@ciq.com>
2025-03-25 13:03:11 -0400

    second example and stageout script

--- level1/lenard-jones-fluid/my-first-input/setup.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/sh
-
-set -x
-ls -lh
-mkdir -p run
-cd run
-wget "https://raw.githubusercontent.com/lammpstutorials/lammpstutorials-inputs/refs/heads/main/level1/lennard-jones-fluid/my-first-input/input.lammps"
-
---
+++ stageout.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+run="${1}"
+dest_prefix="${2}"
+
+tarfile="${run}-${FB_WORKFLOW_ID}.tar.gz"
+tar -czf "${tarfile}" "${run}"
+aws s3 cp  "${tarfile}" "${dest_prefix}/$(basename "${tarfile}")"
