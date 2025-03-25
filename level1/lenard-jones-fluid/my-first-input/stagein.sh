80ad66f49a41e9a1e5d53c9bc689e7a83f5e8968
Wolfgang Resch <wresch@ciq.com>
2025-03-25 14:50:48 -0400

    remove debug info

--- level1/lenard-jones-fluid/my-first-input/stagein.sh
+++ level1/lenard-jones-fluid/my-first-input/stagein.sh
@@ -8,7 +8,3 @@ mkdir -p "${run}" && cd "${run}"
 commit="4e249a6f7a0f8f8057bf72c07d39841a297e69a6"
 url="https://raw.githubusercontent.com/lammpstutorials/lammpstutorials-inputs/${commit}"
 curl -s "${url}/level1/lennard-jones-fluid/my-first-input/input.lammps" > input.lammps
-
-pwd
-ls -lh
-cat input.lammps
