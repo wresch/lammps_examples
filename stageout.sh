db40623fe90947f6ce4f8aa0a36068d29008b5c7
Wolfgang Resch <wresch@ciq.com>
2025-03-25 13:56:30 -0400

    aws cli container does not include tar

--- stageout.sh
+++ stageout.sh
@@ -3,6 +3,4 @@
 run="${1}"
 dest_prefix="${2}"
 
-tarfile="${run}-${FB_WORKFLOW_ID}.tar.gz"
-tar -czf "${tarfile}" "${run}"
-aws s3 cp  "${tarfile}" "${dest_prefix}/${tarfile}"
+aws s3 sync  "${run}" "${dest_prefix}/${run}"
