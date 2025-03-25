d0bdade463259c9e84c56a192eb98af8ae8105b2
Wolfgang Resch <wresch@ciq.com>
2025-03-25 09:56:05 -0400

    fixed setup script for first example

--- level1/lenard-jones-fluid/my-first-input/setup.sh
+++ level1/lenard-jones-fluid/my-first-input/setup.sh
@@ -1,6 +1,8 @@
 #! /bin/sh
 
-mkdir run
+set -x
+ls -lh
+mkdir -p run
 cd run
 wget "https://raw.githubusercontent.com/lammpstutorials/lammpstutorials-inputs/refs/heads/main/level1/lennard-jones-fluid/my-first-input/input.lammps"
 
