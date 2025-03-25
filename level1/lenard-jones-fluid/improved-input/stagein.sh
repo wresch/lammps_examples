6bb9869a465c5ef5d035c0707ab2175e7e9fde40
Wolfgang Resch <wresch@ciq.com>
2025-03-25 14:14:55 -0400

    wget -> curl

--- level1/lenard-jones-fluid/improved-input/stagein.sh
+++ level1/lenard-jones-fluid/improved-input/stagein.sh
@@ -7,6 +7,6 @@ mkdir -p "${run}" && cd "${run}" || exit 1
 
 commit="e249a6f7a0f8f8057bf72c07d39841a297e69a6"
 url="https://raw.githubusercontent.com/lammpstutorials/lammpstutorials-inputs/${commit}"
-wget -O input.lammps "${url}/level1/lennard-jones-fluid/improved-input/input.md.lammps"
-wget -O minimized_coordinate.data "${url}/level1/lennard-jones-fluid/improved-input/minimized_coordinate.data"
+curl "${url}/level1/lennard-jones-fluid/improved-input/input.md.lammps" > input.lammps
+wget "${url}/level1/lennard-jones-fluid/improved-input/minimized_coordinate.data" > minimized_coordinate.data
 
--- level1/lenard-jones-fluid/my-first-input/stagein.sh
+++ level1/lenard-jones-fluid/my-first-input/stagein.sh
@@ -7,4 +7,4 @@ mkdir -p "${run}" && cd "${run}" || exit 1
 
 commit="e249a6f7a0f8f8057bf72c07d39841a297e69a6"
 url="https://raw.githubusercontent.com/lammpstutorials/lammpstutorials-inputs/${commit}"
-wget -O input.lammps "${url}/level1/lennard-jones-fluid/my-first-input/input.lammps"
+curl "${url}/level1/lennard-jones-fluid/my-first-input/input.lammps" > input.lammps
