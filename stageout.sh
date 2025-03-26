00a55511d61aae942116c7ff156e5a226576727e
Wolfgang Resch <wresch@ciq.com>
2025-03-26 15:17:34 -0400

    stageout update

--- stageout.sh
+++ stageout.sh
@@ -2,8 +2,12 @@
 set -ex
 
 run="${1}"
-dest="${2}"
+persist="${2}"
+dest="/output"
 
-if [[ -d  "${run}" && "${dest:-none}" != "none" && -d "${dest}" ]] ; then
+if [[ -d  "${run}" && "${persist}" == "true" && -d "${dest}" ]] ; then
     tar -czf "${dest}/${FB_WORKFLOW_ID}-${run}.tar.gz" "${run}"
+    echo "saved results to ${dest}/${FB_WORKFLOW_ID}-${run}.tar.gz"
+else
+    echo "did not save results to persistent store"
 fi
