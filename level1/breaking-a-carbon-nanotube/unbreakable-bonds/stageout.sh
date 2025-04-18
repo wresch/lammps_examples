417c82ba521064e5a119bb2334e4f2be9aff9936
Wolfgang Resch <wresch@ciq.com>
2025-04-18 18:27:59 -0400

    made each example self contained

---
+++ level1/breaking-a-carbon-nanotube/unbreakable-bonds/stageout.sh
@@ -0,0 +1,13 @@
+#! /bin/sh
+set -ex
+
+run="${1}"
+persist="${2}"
+dest="/output"
+
+if [ -d  "${run}" ] && [ "${persist}" = "true" ] && [ -d "${dest}" ] ; then
+    tar -czf "${dest}/${FB_WORKFLOW_ID}-${run}.tar.gz" "${run}"
+    echo "saved results to ${dest}/${FB_WORKFLOW_ID}-${run}.tar.gz"
+else
+    echo "did not save results to persistent store"
+fi
